fee891f116490bf4a44d4f0444528809ead4ff3e
Rie Amasato <rie_amasato@icloud.com>
2024-04-05 04:03:32 +0000

    ちょっとだけCSS修正

--- fAkePedia/pages/[word]/index.vue
+++ fAkePedia/pages/[word]/index.vue
@@ -1,25 +1,33 @@
 <template>
     fAke Pedia. 
 
+    <h2 class="title">{{title}}</h2>
     <div class="text">
         {{text}}
     </div>
 </template>
 
 <style>
+.title{
+    width: 100%;
+    border-bottom: solid 4px gray;
+}
+
 .text{
-    white-space: pre;
+    white-space: pre-wrap;
 }
 </style>
 
 <script setup>
 const route=useRoute()
 
+const title=ref()
 const text=ref()
 
 onMounted(async()=>{
     text.value="生成中"
-    console.log(route.params.word)
+    title.value=route.params.word
+
     const res=await fetch(`https://rieama.fly.dev/fAkePedia/${route.params.word}`)
     
     text.value=(await(res.json())).text
