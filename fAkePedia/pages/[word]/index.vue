f9515451741ae41c86691b48197d3f8413efcbb7
Rie Amasato <rie_amasato@icloud.com>
2024-04-05 03:51:14 +0000

    ぬxt3を追加

---
+++ fAkePedia/pages/[word]/index.vue
@@ -0,0 +1,28 @@
+<template>
+    fAke Pedia. 
+
+    <div class="text">
+        {{text}}
+    </div>
+</template>
+
+<style>
+.text{
+    white-space: pre;
+}
+</style>
+
+<script setup>
+const route=useRoute()
+
+const text=ref()
+
+onMounted(async()=>{
+    text.value="生成中"
+    console.log(route.params.word)
+    const res=await fetch(`https://rieama.fly.dev/fAkePedia/${route.params.word}`)
+    
+    text.value=(await(res.json())).text
+    console.log(text.value)
+})
+</script>
