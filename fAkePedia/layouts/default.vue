c45cde75583ba9783faf5eadc71531f414f1b511
Rie Amasato <rie_amasato@icloud.com>
2024-04-10 04:05:18 +0000

    広告追加

--- fAkePedia/layouts/default.vue
+++ fAkePedia/layouts/default.vue
@@ -1,3 +1,14 @@
 <template>
     <NuxtPage />
+    <div>
+        <iframe class="ads" src="ads.html" />
+    </div>
 </template>
+
+<style>
+.ads{
+    border: none;
+    width: 100%;
+    height: 50px;
+}
+</style>
